98dde56bbc81f2a8b5b0fbb8803b26fbf792ab31
Matt Isner <matthew.isner@deque.com>
2015-10-08 09:31:49 -0400

    includes alaska-jsa11y version of axe

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,10 @@ var gulp = require('gulp');
 
 var PATH_TO_AXE = 'node_modules/axe-core';
 var AXE_FILE = 'axe.min.js';
+
+var PATH_TO_A11YJS = 'node_modules/a11yjs/dist';
+var A11YJS_FILE = 'alaska-a11yjs.min.js';
+
 var BUILD_DIR = 'dist';
 
 /**
@@ -37,6 +41,11 @@ gulp.task('build', ['clean'], function () {
   var axeSrc  = path.join(PATH_TO_AXE, AXE_FILE);
   var axeDest = path.join(BUILD_DIR, AXE_FILE);
   fs.copySync(axeSrc, axeDest);
+
+  // Copy alaska-jsa11y to dist
+  var jsa11ySrc  = path.join(PATH_TO_A11YJS, A11YJS_FILE);
+  var jsa11yDest = path.join(BUILD_DIR, A11YJS_FILE);
+  fs.copySync(jsa11ySrc, jsa11yDest);
 
   // Render jade files
   gulp.src('src/**/*.jade')
--- src/checks/index.js
+++ src/checks/index.js
@@ -1,12 +1,23 @@
-/* global axe */
+/* global axe, axeXPath */
 
-var opts = {};
+var opts = {
+  reporter: 'v1',
+  runOnly: {
+    type: 'tag',
+    values: ['wcag2a', 'wcag2aa']
+  },
+  checks: {
+    'rowspan': { enabled: false },
+    'headers-visible-text': { enabled: false }
+  }
+};
+
+opts = {};
 
 axe.a11yCheck('html', opts, function (res) {
-  var out = [];
-  res.violations.forEach(function (v) {
-    out.push(v.id);
-  });
-  console.log('Failed rules : ', out);
   console.log('Violations   : ', res.violations);
+});
+
+axeXPath.a11yCheckAsString('html', opts, function (res) {
+  console.log('alaska-jsa11y: ', JSON.parse(res).violations);
 });
